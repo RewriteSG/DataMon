72c5dd77f1b2712d0b68f73e4277e9f55a90f5ae
15-JiaJie <155930454+15-JiaJie@users.noreply.github.com>
2024-01-31 12:32:10 +0800

    did movement and a bit of spawner

---
+++ Assets/Scripts/Behaviours/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] public float MovementSpeed;
+    private Rigidbody2D rb;
+    private Vector2 MovementDirection;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementDirection = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+    }
+    void FixedUpdate()
+    {
+        rb.velocity = MovementDirection * MovementSpeed;
+    }
+}
---
+++ Assets/Scripts/Behaviours/WaveSpawner.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public enum SpawnState { Spawning, Waiting, Counting};
+
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform enemy;
+        public int Count;
+        public float rate;
+    }
+    public Wave[] waves;
+    private int NextWave = 0;
+
+    public float timeBetweenWaves = 5f;
+    public float waveCountdown;
+
+    private SpawnState state = SpawnState.Counting;
+
+    private float searchCountDown = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waveCountdown = timeBetweenWaves;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(state == SpawnState.Waiting)
+        {
+            if(!EnemyIsAlive())
+            {
+                // begin a new round
+
+                WaveCompleted();
+            }
+            else
+            {
+                return;
+            }
+        }
+        if(waveCountdown<=0)
+        {
+            if(state!=SpawnState.Spawning)
+            {
+                //start spawning wave
+                StartCoroutine(SpawnWave(waves[NextWave]));
+            }
+        }
+        else
+        {
+            waveCountdown -= Time.deltaTime;
+        }
+    }
+
+    void WaveCompleted()
+    {
+        Debug.Log("Wave Completed!");
+
+        state = SpawnState.Counting;
+
+        waveCountdown = timeBetweenWaves;
+
+        if (NextWave + 1>waves.Length-1)
+        {
+            NextWave = 0;
+            Debug.Log("ALL WAVES COMPLETE! Looping...");
+        }
+
+        NextWave++;
+    }
+
+    bool EnemyIsAlive()
+    {
+        searchCountDown -= Time.deltaTime;
+        if(searchCountDown<=0f)
+        {
+            searchCountDown = 1f;
+
+            if (GameObject.FindGameObjectWithTag("enemy") == null)
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    IEnumerator SpawnWave(Wave _wave)
+    {
+        Debug.Log("Spawning wave: " + _wave.name);
+
+        state = SpawnState.Spawning;
+
+        for (int i = 0;i<_wave.Count;i++)
+        {
+            SpawnEnemy(_wave.enemy);
+
+            yield return new WaitForSeconds(1f / _wave.rate);
+        }
+
+        state = SpawnState.Waiting;
+
+
+        yield break;
+    }
+    void SpawnEnemy(Transform _enemy)
+    {
+        
+        Debug.Log("Spawning enemy: " + _enemy.name);
+        Instantiate(_enemy, transform.position, transform.rotation);
+    }
+}
