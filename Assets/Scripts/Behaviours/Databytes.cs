168d3742b9450ee507c95bd3bf777d05fd2da1da
15-JiaJie <155930454+15-JiaJie@users.noreply.github.com>
2024-02-05 12:38:03 +0800

    done the thing i was told to do
    made capsule item spawn on another item being destroyed, increase databyte_Count upon touching the item

---
+++ Assets/Scripts/Behaviours/Databytes.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Databytes : MonoBehaviour
+{
+    public GameObject Data_bytes;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            
+            Instantiate(Data_bytes, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+
+        }
+    }
+}
