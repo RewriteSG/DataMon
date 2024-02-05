48cb4a766d5ea8fd1928c6cb536755e07a94819f
Halal Hamster <mhazwan112@gmail.com>
2024-02-05 13:51:29 +0800

    Updated Scripts
    DataDex Scripts and added DataMon prefab

--- Assets/Scripts/Behaviours/Player/DataDex.cs
+++ Assets/Scripts/Behaviours/Player/DataDex.cs
@@ -1,16 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+using UnityEngine.UI;
 public class DataDex : MonoBehaviour
 {
     public static DataDex instance;
+    public Transform Content;
+    public GameObject Label, DataMonContainer;
     public List<DataMonsData> AllDataMons = new List<DataMonsData>();
+    List<GameObject> DataMonListInDex = new List<GameObject>();
     public List<DataMonHolder> CompanionsDataMon = new List<DataMonHolder>();
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
+        int DataMonEvoCount;
+        GameObject temp;
+        for (int i = 0; i < AllDataMons.Count; i++)
+        {
+            DataMonEvoCount = AllDataMons[i]._DataMon.Length;
+            for (int x = 0; x < DataMonEvoCount; x++)
+            {
+                temp = Instantiate(Label, Content);
+                temp.GetComponentInChildren<TextMeshProUGUI>().text = "Datamon #"+x+" : " + AllDataMons[i]._DataMon[x].DataMonName;
+
+            }
+        }
     }
 
     // Update is called once per frame
